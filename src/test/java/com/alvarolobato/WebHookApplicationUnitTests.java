1e2d6caae470403a7e5e2f06bfffc2601b91ab05
alvarolobato <alvarolobato@gmail.com>
2018-01-18 00:41:46 +0100

    #10 Test deletions are handled and others ignored

--- src/test/java/com/alvarolobato/WebHookApplicationUnitTests.java
+++ src/test/java/com/alvarolobato/WebHookApplicationUnitTests.java
@@ -1,25 +1,22 @@
 package com.alvarolobato;
 
+import static com.alvarolobato.TestUtils.loadFromFile;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
 
 public class WebHookApplicationUnitTests {
 
     @Test
     public void validSignatureMessageAcceptedTest() throws IOException {
         WebHookApplication app=new WebHookApplication();
-        String signature = loadFromFile("repo2-deletion-event2-sha.txt");
-        ResponseEntity<String> response = app.handle(signature, loadFromFile("repo2-deletion-event.json"));
+        String signature = loadFromFile("repo2-deletion-event-sha.txt");
+        ResponseEntity<String> response = app.handleRequest(signature, loadFromFile("repo2-deletion-event.json"));
         assertThat("Call should have succeded", response.getStatusCode(), equalTo(HttpStatus.OK));
     }
 
@@ -27,7 +24,7 @@ public class WebHookApplicationUnitTests {
     public void invalidSignatureMessageForbidenTest() throws IOException {
         WebHookApplication app = new WebHookApplication();
         String signature = "randomstring";
-        ResponseEntity<String> response = app.handle(signature, loadFromFile("repo2-deletion-event.json"));
+        ResponseEntity<String> response = app.handleRequest(signature, loadFromFile("repo2-deletion-event.json"));
         assertThat("Call should have succeded", response.getStatusCode(), equalTo(HttpStatus.OK));
     }
 
@@ -35,11 +32,7 @@ public class WebHookApplicationUnitTests {
     public void nullSignatureMessageForbidenTest() throws IOException {
         WebHookApplication app = new WebHookApplication();
         String signature = "randomstring";
-        ResponseEntity<String> response = app.handle(signature, loadFromFile("repo2-deletion-event.json"));
+        ResponseEntity<String> response = app.handleRequest(signature, loadFromFile("repo2-deletion-event.json"));
         assertThat("Call should have succeded", response.getStatusCode(), equalTo(HttpStatus.OK));
-    }
-
-    public static String loadFromFile(String name) throws IOException {
-        return StreamUtils.copyToString(WebHookApplicationUnitTests.class.getResourceAsStream(name), Charset.forName("UTF-8"));
     }
 }
